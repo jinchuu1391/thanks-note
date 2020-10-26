f182898a37139688ade31ec182bed8d557becf6f
jinchuu1391 <62422486+jinchuu1391@users.noreply.github.com>
2020-10-26 16:18:51 +0900

    로그인 확인 및 토큰 검증 미들웨어 구현

--- helper/checkLoggedIn.js
+++ helper/checkLoggedIn.js
@@ -1,4 +1,18 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const checkLoggedIn = (request, response, next) => {
   // 토큰을 검증하고 로그인 여부를 확인하는 미들웨어
+  let token = request.cookies["access-token"];
+  if (!token) {
+    response.status(401).send("로그인이 필요합니다");
+  }
+  try {
+    let decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    request.decoded = decoded;
+    next();
+  } catch (error) {
+    response.status(401).send("토큰에러!");
+  }
 };
 module.exports = checkLoggedIn;
